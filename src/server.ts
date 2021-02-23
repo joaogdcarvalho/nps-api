4cd6f6edf9d315508c3df538f414e1c47925a360
joaogab <joaogabrieldecarvalho.com@gmail.com>
2021-02-23 01:29:24 -0300

    fundamentos do nodejs
    criar rota post

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,12 @@ import express from 'express';
 
 const app = express();
 
-app.get("/users", (request, response) => {
+app.get("/", (request, response) => {
   return response.json({ message: "Hello World!"});
+});
+
+app.post("/", (request, response) => {
+  return response.json({ message: "Os dados foram salvos com sucesso!"})
 });
 
 app.listen(3333, () => console.log("Server is running!"));
